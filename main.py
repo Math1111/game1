87286ebdc2f185c32e420d0062b9888687f2652a
Math1111 <emelyanov2704@mail.ru>
2024-12-15 15:51:50 +0300

    +

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import menu
 
 def set_status(text, color='black'):
     canvas.itemconfig(text_id, text=text, fill=color)
@@ -13,16 +14,17 @@ def key_handler(event):
 
     if game_over:
         return
-    #if event.keycode == KEY_PAUSE:
-    #    pause_toggle()
+    if event.keycode == KEY_PAUSE:
+        menu.pause_toggle()
+        set_status('Пауза')
 
-    #if pause:
-    #    return
-    #if event.keycode == KEY_ESC:
-    #    menu_toggle()
+    if menu.pause:
+        return
+    if event.keycode == KEY_ESC:
+        menu.menu_toggle(canvas)
 
-    #if menu_mode:
-    #    return
+    if menu.menu_mode:
+        return
 
     set_status('Вперед!')
     if event.keycode == KEY_PLAYER1:
@@ -60,7 +62,7 @@ KEY_ENTER = 13
 
 KEY_PLAYER1 = 39
 KEY_PLAYER2 = 68
-KEY_PAUSE = 19
+KEY_PAUSE = 32
 
 SPEED = 12
 
@@ -79,7 +81,7 @@ window.title('Меню игры')
 
 canvas = Canvas(window, width=game_width, height=game_height, bg='white')
 canvas.pack()
-#menu_create(canvas)
+menu.menu_create(canvas)
 player1 = canvas.create_rectangle(x1, y1, x1 + player_size, y1 + player_size, fill=player1_color)
 player2 = canvas.create_rectangle(x2, y2, x2 + player_size, y2 + player_size, fill=player2_color)
 finish_id = canvas.create_rectangle(x_finish, 0, x_finish + 10, game_height, fill='black')
